079286b613e35931571b713e668b7e22fc2ad5fc
Zubair Irfan Khan <zubair.khan@airlifttech.com>
2022-07-16 15:12:44 +0500

    completed project

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -14,11 +14,27 @@ class ViewController: UIViewController {
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
     
+    var storyBrain = StoryBrain()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        updateUI()
 
     }
 
-
+    @IBAction func choiceMade(_ sender: UIButton) {
+        let userSelection = sender.currentTitle!
+        storyBrain.nextStory(userChoice: userSelection)
+        updateUI()
+    }
+    
+    func updateUI(){
+        let story = storyBrain.getCurrentStory()
+        storyLabel.text = story.title
+        choice1Button.setTitle(story.choice1, for: .normal)
+        choice2Button.setTitle(story.choice2, for: .normal)
+    }
+    
 }
 
